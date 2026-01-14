8dbe2eb5df6ed2ba6b7f685bfa544d30bf52f1a0
Allisson dos Santos do Prado <120612811+alissontebuias@users.noreply.github.com>
2026-01-14 00:12:23 -0400

    Refactor time display function and improve readability

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-function carregar() {
-    var msg = document.getElementById('msg')
-    var img = document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
-    var minutos = data.getMinutes()
-    var seconds = data.getSeconds()
-    msg.innerHTML = `A hora certa é ${hora}:${minutos}:${seconds}.`
-    if (hora >= 0 && hora < 12) {
-        //bom dia
-        img.src = 'manha.jpg'
-    } else if (hora >= 12 && hora < 18) {
-        //boa tarde
-        img.src = 'tarde.jpg'
+function atualizarRelogio() {
+    const msg = document.getElementById('msg');
+    const img = document.getElementById('imagem');
+    const agora = new Date();
+
+    let hora = agora.getHours();
+    let minutos = agora.getMinutes();
+    let segundos = agora.getSeconds();
+
+    hora = hora.toString().padStart(2, '0');
+    minutos = minutos.toString().padStart(2, '0');
+    segundos = segundos.toString().padStart(2, '0');
+
+    msg.innerHTML = `A hora certa é ${hora}:${minutos}:${segundos}.`;
+
+    if (hora < 12) {
+        img.src = 'manha.jpg';
+    } else if (hora < 18) {
+        img.src = 'tarde.jpg';
     } else {
-        //boa noite
-        img.src = 'noite.jpg'
-    
-    
+        img.src = 'noite.jpg';
     }
-    setTimeout(carregar, 1000)
+
+    setTimeout(atualizarRelogio, 1000);
 }
+
+atualizarRelogio();
